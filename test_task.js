6488b5d7ab5c883b3b2919598b2f00f4611097ea
kukla1989 <kukla.1989@ukr.net>
2020-12-03 18:46:55 +0200

    add function that formatting data in require way

--- test_task.js
+++ test_task.js
@@ -3,16 +3,64 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 var data = [];
+var newData = [];
 
 
 fs.createReadStream('acme_worksheet.csv')
   .pipe(csv())
   .on('data', (row) => {
-  		//console.log(row['Work Hours']);
  		data.push(row);
   })
-  .on('end', () => {
-      console.log( data[2]['Work Hours']) //testing
+  .on('end', () => {				//here we get data from csv file
+      transformData(data)
   });
+
+
+
+
+  function transformData(data){
+ 	let firstIteration = true;
+ 	data.forEach((object, index)=>{
+ 		if(firstIteration || typeof findObjectbyName(object['Employee Name'], newData) == 'undefined'){  //only after first iteration we need add property emloyee name
+			newData.push({
+				"Employee Name": object["Employee Name"],
+				[object['Date']]: object["Work Hours"] 
+				});
+			 if (object["Employee Name"]  == 'Luann Cooper') firstIteration = false;
+ 		} else {
+ 		 	findObjectbyName(Object.values(object)[0], newData)[object.Date] = object['Work Hours'];
+ 		 	console.log(newData.find(obj => {return obj["Employee Name"] === Object.values(object)[0]}));
+ 		}
+
+ 	})
+ 	return inputOInData(newData);   //put 0 hours  if there are not record
+  }
+
+
+
+
+ function findObjectbyName(name, data){
+ 	return data.find(obj => {
+ 		 		return obj["Employee Name"] === name
+ 		 	})
+ }
+
+
+
+
+ function inputOInData(data){
+ 	data.forEach(object => {
+ 		if(Object.values(object).length < 7){
+ 			if (typeof object['Jun 29 2020'] == 'undefined')  object['Jun 29 2020'] = 0;
+ 			if (typeof object['Jun 30 2020'] == 'undefined')  object['Jun 30 2020'] = 0;
+ 			if (typeof object['Jul 01 2020'] == 'undefined')  object['Jul 01 2020'] = 0;
+ 			if (typeof object['Jul 02 2020'] == 'undefined')  object['Jul 02 2020'] = 0;
+ 			if (typeof object['Jul 03 2020'] == 'undefined')  object['Jul 03 2020'] = 0;
+ 			if (typeof object["Jul 04 2020"] == 'undefined')  object["Jul 04 2020"] = 0;
+ 			if (typeof object['Jul 05 2020'] == 'undefined')  object['Jul 05 2020'] = 0;
+ 		} 		
+ 	})
+ 	return data;
+ }
  
   
